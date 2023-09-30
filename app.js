33bd36dc46837c70ac5a4289fee05a0145b5080b
Shashank Naik <shashanknaik808@gmail.com>
2023-09-30 16:09:26 +0530

    Verifying User Identity

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const router = require("./routes/user-routes.js");
 
 const app = express();
 
-mongoose.connect('mongodb+srv://shashanknaik808:qwerty123456@cluster0.nzvwcqv.mongodb.net/Blog?retryWrites=true&w=majority')
+app.use("/api/user" ,router)
+
+mongoose
+    .connect('mongodb+srv://shashanknaik808:qwerty123456@cluster0.nzvwcqv.mongodb.net/Blog?retryWrites=true&w=majority'
+    )
     .then(() => app.listen(5000))
     .then(() =>
-        console.log("Connected TO Database and Listening TO Localhost 5000"))
+        console.log("Connected TO Database and Listening TO Localhost 5000")
+    )
     .catch((err) => console.log(err));
