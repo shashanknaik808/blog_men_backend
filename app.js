5a8d23f63e620df012bdbfc82b5d409ec1ed243f
Shashank Naik <shashanknaik808@gmail.com>
2023-09-30 15:23:18 +0530

    Installing Mongoose and Connecting to MongoDB

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 
-app.use("/api", (req, res, next) => {
-    res.send("Hello World");
-});
-
-app.listen(5000);
+mongoose.connect('mongodb+srv://shashanknaik808:qwerty123456@cluster0.nzvwcqv.mongodb.net/Blog?retryWrites=true&w=majority')
+    .then(() => app.listen(5000))
+    .then(() =>
+        console.log("Connected TO Database and Listening TO Localhost 5000"))
+    .catch((err) => console.log(err));
