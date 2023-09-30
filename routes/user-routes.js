69a64f1ae54ab670566b6aeacb8573b682d7172a
Shashank Naik <shashanknaik808@gmail.com>
2023-09-30 16:46:08 +0530

    Adding SignUp request

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -4,5 +4,6 @@ const getAllUser = require('../controllers/user-controller');
 const router = express.Router();
 
 router.get("/", getAllUser);
+router.post("/signup", signup)
 
 module.exports = router;
