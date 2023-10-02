7a0e269780c5d2baf36f050ee6cf9f6ca52e44d3
Shashank Naik <shashanknaik808@gmail.com>
2023-10-02 15:56:58 +0530

    Login Function Successful

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -55,8 +55,7 @@ module.exports.signup = async (req, res, next) => {
 };
 
 module.exports.login = async (req, res, next) => {
-    const { name, email, password } =
-        req.body;
+    const { email, password } = req.body;
 
     let existingUser;
     try {
@@ -67,9 +66,20 @@ module.exports.login = async (req, res, next) => {
     catch (err) {
         return console.log(err);
     }
-    if (existingUser) {
+    if (!existingUser) {
         return res
             .status(404)
             .json({ message: "Couldnt Find User By This Email" });
     }
+
+    const isPasswordCorrect = bcrypt.compareSync(password, existingUser.password);
+
+    if (!isPasswordCorrect) {
+        return res
+        .status(400)
+        .json({ message: "Incorrect Password" })
+    }
+    return res
+        .status(200)
+        .json({ message: "Login Successful" });
 }
